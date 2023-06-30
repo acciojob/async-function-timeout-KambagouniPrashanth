349ab40d835ea207a80f0799a9aacbd3fef6f81f
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-06-30 10:17:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 //your JS code here. If required.
  
+const formelemnt=document.querySelector("form")
 
 function myGreeting() {
-	const formelemnt=document.querySelector("form")
-        console.log(formelemnt[1].id)
-        console.log(formelemnt[0].id)
-        function time(){
-            document.setTimeout(() => {
-               console.log(formelemnt[0].innerText) ;
-                
-            },formelemnt[1].value);
-            console.log()
+        // console.log(formelemnt[1].id)
+        // console.log(formelemnt[0].id)
+	formelemnt[0]=formelemnt[0]
+}
+ window.setTimeout(myGreeting,formelemnt[1])
 
-        }
+
+
+// 	function callme(){
+// const divElement=document.getElementById("output")
+// divElement.innerHTML="Hello, world!"
+// }
+// window.setTimeout(callme,1000)
 
 	
 }

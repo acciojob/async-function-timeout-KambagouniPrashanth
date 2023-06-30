f5bec0a7776128a77adc8862f66da0297bde62f5
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-06-30 10:05:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+ const formelemnt=document.querySelector("form")
+        console.log(formelemnt[1].id)
+        console.log(formelemnt[0].id)
+        function time(){
+            document.setTimeout(() => {
+               console.log(formelemnt[0].innerText) ;
+                
+            },formelemnt[1].value);
+            console.log()
+
+        }
+
+
+function myGreeting() {
+	const target=document.getElementByTag("input")[1];
+	const button=document.getElementBy
+	
+}

7e3da94712c22e1e1b562cd66415dfec3ec4990b
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-07-17 11:36:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,20 @@
-// //your JS code here. If required.
-// const inputText=document.getElementById("text")
-// const delayNumber=document.getElementById("delay")
-// const btn=document.getElementById("btn")
-// const divElement=document.getElementById("output")
-
-// async function myGreeting(){
-
-//     await new Promise((resolve)=>{
-//         setTimeout(()=>{
-//           resolve()
-//         },delayNumber.value)
-//     })
-//     divElement.innerText=inputText.value;
-// }
-// btn.addEventListener("click",()=>{
-//     myGreeting()
-// })
-
- let inputText=document.getElementById("text");
-let delaytime=document.getElementById("delay")
-let output=document.getElementById("output");
-let btn=document.getElementById("btn");
+//your JS code here. If required.
+const inputText=document.getElementById("text")
+const delayNumber=document.getElementById("delay")
+const btn=document.getElementById("btn")
+const divElement=document.getElementById("output")
 
 async function myGreeting(){
-await new Promise((resolve,reject)=>{
-setTimeout(()=>{
-	resolve()
 
-},delaytime.value)
-
-		output.innerText=inputText.value;
-
-	})
+    await new Promise((resolve)=>{
+        setTimeout(()=>{
+          resolve()
+        },delayNumber.value)
+    })
+    divElement.innerText=inputText.value;
 }
-
-btn.addEventListener("click",myGreeting)
+btn.addEventListener("click",()=>{
+    myGreeting()
+})
 
 
